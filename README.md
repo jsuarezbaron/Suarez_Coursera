011c06fc833ee9a304456d13b291763dd01ca906
jsuarezbaron <jsuarezbaron@gmail.com>
2017-11-01 17:59:30 -0300

    Creating Initial Coursera Repository

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Suarez_Coursera
 Repository for Week 1 Application Assignment of Introduction to Embedded Systems Software and Development Environments  Course.
+
+@autor Juan Carlos Suárez Barón
+* This is the final code for the Week 1 Application Assignment.
+* Consist on eight functions:
---
+++ stats.h
@@ -0,0 +1,97 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file <Add File Name> 
+ * @brief <Add Brief Description Here >
+ *
+ * <Add Extended Description Here>
+ *
+ * @author <Add FirsName LastName>
+ * @date <Add date >
+ *
+ */
+#ifndef __STATS_H__
+#define __STATS_H__
+
+/* Add Your Declarations and Function Comments here */ 
+
+/**
+ * @brief <Add Brief Description of Function Here>
+ *
+ * <Add Extended Description Here>
+ *
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ *
+ * @return <Add Return Informaiton here>
+ */
+
+
+/**
+*@brief A function that prints the statistics of an array including minimum, maximum, mean, and median.
+*
+*
+*@return void
+*/
+//void print_statistics();
+
+/**
+*@brief  Given an array of data and a length, prints the array to the screen.
+*
+*<Add Extended Description Here>
+*
+*@return
+*/
+///void print_array();
+
+/**
+*@brief Given an array of data and a length, returns the median value.
+*
+*
+*@return
+*/
+///void find_median();
+
+/**
+*@brief Given an array of data and a length, returns the mean.
+*
+*
+*@return
+*/
+///void find_mean();
+
+/**
+*@brief Given an array of data and a length, returns the maximum.
+*
+*
+*@return maximum
+*/
+///void find_maximum();
+
+/**
+*@brief Given an array of data and a length, returns the minimum. 
+*
+*
+*@return
+*/
+///void find_minimum();
+
+/**
+*@brief  Given an array of data and a length, sorts the array from largest to smallest. 
+*		(The zeroth Element should be the largest value, and the last element (n-1) should be the smallest value. )
+*
+*
+*@return
+*/
+///void sort_array();
+#endif /* __STATS_H__ */
